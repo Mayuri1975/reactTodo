ec960bec9fc5b2c4078e5a5d50bc493ce07c3e93
Mayuri Sure <141281357+Mayuri1975@users.noreply.github.com>
2024-10-07 16:46:44 +0530

    something went wrong

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import Tod from './components/Tod';
+import { ShimmerText } from "react-shimmer-effects";
+
+
+function App() {
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <div>
+    
+          <ShimmerText line={23} gap={20} />;
+        </div>
+      ) : (
+        <Tod />
+      )}
+    </>
+  );
+}
+
+export default App;
